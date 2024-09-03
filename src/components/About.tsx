94f28d3e0fd31f1d943036e868f48b7a60a03e76
Kanuck-mk2 <141983009+Kanuck-mk2@users.noreply.github.com>
2024-09-02 23:03:44 -0600

    update

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,7 +23,7 @@ const About: React.FC = () => {
 
       <div className="flex flex-col md:flex-row justify-between items-center ">
         <motion.div
-          className="w-full md:w-2/4 rounded-lg flex justify-center mb-6 mr-5"
+          className="w-full md:w-2/4 rounded-lg flex justify-center mb-6 mr-10"
           {...aboutImage}
         >
           <motion.div
